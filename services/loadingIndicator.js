6056f6852c0f2965528d2bbeae7188f71fdceee0
Tal-Brodkin <tal.brodkin@gmail.com>
2024-05-23 19:29:00 +0300

    location permission screen, switch between users photos, gitignore updated

--- services/loadingIndicator.js
+++ services/loadingIndicator.js
@@ -2,19 +2,16 @@ import React from 'react';
 import {ActivityIndicator, StyleSheet, View} from 'react-native';
 
 const Loader = () => (
-  <View style={[styles.container, styles.centered]}>
+  <View style={styles.container}>
     <ActivityIndicator size="large" color="#0000ff" />
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
-  },
-  centered: {
     alignItems: 'center',
-  },
+  }
 });
 
 export default Loader;
